1016aa0e21f4eee0c227b95bec4b4341d3798d56
andre <ahochschulte.apple@gmail.com>
2021-06-06 11:44:20 +0200

    Final Commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "50488f5cbcf048479c27b0479330dd9d"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
